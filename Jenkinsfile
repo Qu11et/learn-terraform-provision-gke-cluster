4ff2b1d0afb0c6bfa2f3677112b1a87c590c701d
Tai Khau <TaiKhau@tai-dev.asia-southeast1-b.c.solar-modem-471003-a8.internal>
2025-11-20 11:57:55 +0000

    Test jenkins #1

---
+++ Jenkinsfile
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+
+    environment {
+        PROJECT_ID = 'gcp-project-id'
+        REGION     = 'gke-region'
+        CLUSTER_NAME = 'gke-cluster-name'
+        
+        GCP_CRED_ID = 'gcp-service-account-key' 
+    }
+
+    triggers {
+        githubPush() 
+    }
+
+    stages {
+        stage('Terraform Apply') {
+            steps {
+                script {
+                    withCredentials([file(credentialsId: GCP_CRED_ID, variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+                        sh 'terraform init'
+                        sh 'terraform plan -out=tfplan'
+                        sh 'terraform apply -auto-approve tfplan'
+                    }
+                }
+            }
+        }
+
+        stage('Connect to GKE') {
+            steps {
+                withCredentials([file(credentialsId: GCP_CRED_ID, variable: 'GC_KEY')]) {
+                    script {
+                        sh 'gcloud auth activate-service-account --key-file=$GC_KEY'
+                        sh "gcloud config set project ${PROJECT_ID}"
+                        
+                        sh "gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION}"
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                // Dọn dẹp workspace
+                cleanWs()
+            }
+        }
+        failure {
+            echo 'Pipeline failed. Please check the logs.'
+        }
+    }    
+}
--- gke.tf
+++ gke.tf
@@ -18,37 +18,47 @@ variable "gke_num_nodes" {
 
 # GKE cluster
 data "google_container_engine_versions" "gke_version" {
-  location = var.region
-  version_prefix = "1.27."
+  location       = "${var.region}-a"
+  project        = var.project_id
+  version_prefix = "1.32.9-gke.1130000"
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
-  location = var.region
+  name     = "${var.project_id}-gke2"
+  location = "${var.region}-a"
+  project  = var.project_id
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
+  # remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  min_master_version = data.google_container_engine_versions.gke_version.latest_master_version
 }
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
-  location   = var.region
+  location   = "${var.region}-a"
   cluster    = google_container_cluster.primary.name
+  project  = var.project_id
   
-  version = data.google_container_engine_versions.gke_version.release_channel_default_version["STABLE"]
+  version = data.google_container_engine_versions.gke_version.latest_node_version
   node_count = var.gke_num_nodes
 
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
 
     labels = {
@@ -57,6 +67,8 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # preemptible  = true
     machine_type = "n1-standard-1"
+    disk_size_gb = 32
+    disk_type    = "pd-standard"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
